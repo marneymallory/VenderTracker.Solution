cc6b4a2dddb84a86ea45d3aa6a8d125652f26848
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-05-14 12:42:26 -0800

    add passing test for GetVendorDescription

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -6,23 +6,36 @@ using VendorTracker.Models;
 namespace VendorTracker.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("test vendor");
+      Vendor newVendor = new Vendor("test", "test");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
     [TestMethod]
     public void GetVendorName_ReturnsVendorName_String()
     {
-      string name= "Sally's Diner";
-      Vendor newVendor = new Vendor(name);
+      string name = "Sally's Diner";
+      Vendor newVendor = new Vendor(name, "test");
 
       string result = newVendor.Name;
 
       Assert.AreEqual(name, result);
+    }
+    [TestMethod]
+    public void GetVendorDescription_ReturnsVendorDescription_String()
+    {
+      string description = "Sally's Diner: 24-hour comfort food";
+      Vendor newVendor = new Vendor("Sally's Diner", description);
+      string result = newVendor.Description;
+      Assert.AreEqual(description, result);
     }
   }
 }

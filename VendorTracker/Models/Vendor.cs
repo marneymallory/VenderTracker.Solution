5b785c1c5f0c7e5348de9f54fb37eaaf7577a093
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-05-14 13:35:09 -0800

    add passing test for GetId

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -4,24 +4,22 @@ namespace VendorTracker.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
     public int Id { get; }
+    public string Description { get; set; }
 
-    public Vendor(string vendorName)
-    {
-      Name = vendorName;
-      _instances.Add(this);
-      Id = _instances.Count;
-      Orders = new List<Order>{};
-    }
-     public static List<Vendor> GetAll()
-    {
-      return _instances;
-    }
 
-    public static Vendor Find(int searchId)
+    public Vendor(string name, string description)
     {
-      return _instances[searchId - 1];
+      Name = name;
+       _instances.Add(this);
+      Description = description; 
+      Id = _instances.Count; 
+      }
+      public static void ClearAll()
+    {
+      _instances.Clear();
+    }
     }
   }
-}

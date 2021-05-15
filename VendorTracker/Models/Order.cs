dc055a701dc90d3b5764367411ebdcfadf65cf65
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-05-14 16:18:44 -0800

    add good fail for GetOrderTitle

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -5,5 +5,12 @@ namespace VendorTracker.Models
   public class Order
   {
     public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+    public Order(string title, string description, int price, string date)
+    {
+
+    }
   }
 }

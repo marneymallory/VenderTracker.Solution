90df8cf0d7e5718f38de70d64643325a925d567d
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-05-15 23:22:44 -0800

    add webpage functionality

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -25,5 +25,27 @@ namespace VendorTracker.Controllers
      Vendor newVendor = new Vendor(vendorName, vendorDescription);
      return RedirectToAction("Index");
     }
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> vendorOrderDictionary = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      vendorOrderDictionary.Add("vendor", selectedVendor);
+      vendorOrderDictionary.Add("orders", vendorOrders);
+      return View(vendorOrderDictionary);
+    }
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string title, string description, int price, string date)
+    {
+      Dictionary<string, object> vendorOrderDictionary = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(title, description, price, date);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      vendorOrderDictionary.Add("orders", vendorOrders);
+      vendorOrderDictionary.Add("vendor", foundVendor);
+      return View("Show", vendorOrderDictionary);
+    }
   }
 }

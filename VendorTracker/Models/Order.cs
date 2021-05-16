79e8b1914d304b6f6e551f46a9a493e75f5b7b87
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-05-15 20:18:13 -0800

    add passing test for FindId in OrderTests.cs

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -22,7 +22,7 @@ namespace VendorTracker.Models
     }
      public static Order Find(int searchId)
       {
-        return _instances[searchId];
+        return _instances[searchId - 1];
       }
     public static void ClearAll()
     {
